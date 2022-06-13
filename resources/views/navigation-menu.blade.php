efda374f3068cca7028bf458ecd0e77f2207313a
fewpixels <74591245+fewpixels@users.noreply.github.com>
2022-06-13 10:24:22 +0200

    super basic push
    basic contat

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -13,7 +13,10 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                        {{ __('het loopt als een tiet') }}
+                    </x-jet-nav-link>
+                    <x-jet-nav-link href="{{ route('contact') }}" :active="request()->routeIs('contact')">
+                        {{ __('contact') }}
                     </x-jet-nav-link>
                 </div>
             </div>
--- routes/web.php
+++ routes/web.php
@@ -25,4 +25,10 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+
+    Route::get('/contact', function () {
+        return view('contact');
+    })->name('contact');
+
 });
+
