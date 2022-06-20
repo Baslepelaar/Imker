990df11e5c50dc9cfe17b8686d2bb70e1c9a4834
MathieuvanBeek <74538092+MathieuvanBeek@users.noreply.github.com>
2022-06-20 12:25:33 +0200

    Basiscursus pagina aangepast

--- resources/views/basiscursus.blade.php
+++ resources/views/basiscursus.blade.php
@@ -5,9 +5,41 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <img src="basiscursus_2013_1.jpg" alt="Basiscursus 2013" width="500" height="600">
+    <section class="container py-12">
+        <div class="row max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <article class="col-md-6">
+                <p>Overweegt U om bijen te gaan houden? Dan is de basiscursus bijen houden een aanrader om goed
+                    voorbereid
+                    van start te gaan.
+                    De imkervereniging Oegstgeest geeft jaarlijks de basiscursus Imkeren.
+                    De cursus bestaat uit 5 theorielessen en 12 praktijklessen.
+                    Theorielessen worden in de avond gegeven van 19:30 tot 21:30 en beginnen in februari.
+                    Praktijklessen zijn gepland op zaterdagmiddag tussen 13:00 tot 16:30 en beginnen in april. Omdat
+                    praktijklessen afhankelijk zijn van het weer, kunnen geplande cursus middagen afwijken.
+                    De cursus wordt afgesloten met een schriftelijk examen, waarna u een diploma krijgt.
+                    Onderstaand is inbegrepen in het cursusgeld en wordt bij de eerste les uitgereikt:
+
+                <ul style="list-style-type:disc;">
+                    <li>Het lesboek</li>
+                    <li>Het praktijkboek</li>
+                </ul>
+
+                basiscursus 2013
+                Tijdens het jaar waarin u de cursus volgt, krijgt u gratis het lidmaatschap van de NBV aangeboden en
+                ontvangt u maandelijks het blad Bijenhouden.
+                De cursuskosten bedragen € 200,- per deelnemer. Het maximum aantal deelnemers is 12 per cursus. Bij meer
+                aanmeldingen dan beschikbare plaatsen, geldt de volgorde van aanmelding, u wordt hier over
+                geïnformeerd.</p>
+
+                <img src="img/basiscursus_2013_1.jpg" alt="Basiscursus 2013" width="500" height="600">
+            </article>
         </div>
-    </div>
+    </section>
+
+    <section>
+        <article>
+            <h1>Inschrijven</h1>
+
+        </article>
+    </section>
 </x-guest-layout>
