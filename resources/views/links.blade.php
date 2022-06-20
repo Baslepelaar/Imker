d822f1daa259f0ae4f005d3411e37574a8e44b9b
MathieuvanBeek <74538092+MathieuvanBeek@users.noreply.github.com>
2022-06-20 09:27:38 +0200

    Links pagina af

--- resources/views/links.blade.php
+++ resources/views/links.blade.php
@@ -20,19 +20,19 @@
                     <tbody>
                     <tr>
                         <td>De landelijke bijenvereniging.</td>
-                        <td class="website-links"><a href="https://www.bijenhouders.nl/">Bijenhouders</a></td>
+                        <td><a href="https://www.bijenhouders.nl/" target="_blank">Bijenhouders</a></td>
                     </tr>
                     <tr>
                         <td>Archief van het gedigitaliseerde imkerblad vanaf de oprichting (1898) tot 2011.</td>
-                        <td><a href="https://www.bijenhouden.nl/index.php?option=com_content&view=article&id=105&Itemid=71">Bijenhouden</a></td>
+                        <td><a href="https://www.bijenhouden.nl/index.php?option=com_content&view=article&id=105&Itemid=71" target="_blank">Bijenhouden</a></td>
                     </tr>
                     <tr>
                         <td>Het bijenforum, een forum waar veel kennis -en soms onzin te lezen is.</td>
-                        <td><a href="https://www.bijenhouden.nl/">Bijenhouden</a></td>
+                        <td><a href="https://www.bijenhouden.nl/" target="_blank">Bijenhouden</a></td>
                     </tr>
                     <tr>
                         <td>Imkerpedia over bijenkasten.</td>
-                        <td><a href="https://www.imkerpedia.nl/wiki/index.php?title=Bijenkasten">Imkerpedia</a></td>
+                        <td><a href="https://www.imkerpedia.nl/wiki/index.php?title=Bijenkasten" target="_blank">Imkerpedia</a></td>
                     </tr>
                     </tbody>
                 </table>
@@ -46,20 +46,32 @@
                     </thead>
                     <tbody>
                     <tr>
-                        <td>De landelijke bijenvereniging.</td>
-                        <td class="website-links"><a href="https://www.bijenhouders.nl/">Bijenhouders</a></td>
+                        <td>Wikipedia over de aardhommel.</td>
+                        <td><a href="https://nl.wikipedia.org/wiki/Aardhommel_(soort)" target="_blank">De Aardhommel</a></td>
                     </tr>
                     <tr>
-                        <td>Archief van het gedigitaliseerde imkerblad vanaf de oprichting (1898) tot 2011.</td>
-                        <td><a href="https://www.bijenhouden.nl/index.php?option=com_content&view=article&id=105&Itemid=71">Bijenhouden</a></td>
+                        <td>Wikipedia over de “bumblebee”.</td>
+                        <td><a href="https://en.wikipedia.org/wiki/Bumblebee" target="_blank">De Bumblebee</a></td>
                     </tr>
                     <tr>
-                        <td>Het bijenforum, een forum waar veel kennis -en soms onzin te lezen is.</td>
-                        <td><a href="https://www.bijenhouden.nl/">Bijenhouden</a></td>
+                        <td>“Drachtplanten” zijn het lezen waard, je weet dan waar de bijen de honing vandaan halen.</td>
+                        <td><a href="http://www.drachtplanten.nl/" target="_blank">Drachtplanten</a></td>
                     </tr>
                     <tr>
-                        <td>Imkerpedia over bijenkasten.</td>
-                        <td><a href="https://www.imkerpedia.nl/wiki/index.php?title=Bijenkasten">Imkerpedia</a></td>
+                        <td>De Nederlandse bijen en hun relaties.</td>
+                        <td><a href="https://www.wildebijen.nl/" target="_blank">Wildebijen</a></td>
+                    </tr>
+                    <tr>
+                        <td>“Tuinwild” over solitaire en sociale bijen</td>
+                        <td><a href="http://www.tuinwild.nl/bij.htm" target="_blank">Tuinwild</a></td>
+                    </tr>
+                    <tr>
+                        <td>“Wageningen University & Research” over bijen. Met nieuwsbrief.</td>
+                        <td><a href="https://www.wur.nl/nl/Landingspagina-redacteuren/nl/Onderzoek-Resultaten/Onderzoeksinstituten/plant-research/Biointeracties-Plantgezondheid/Bijen.htm" target="_blank">Bijen</a></td>
+                    </tr>
+                    <tr>
+                        <td>Bijentaal in beeld, Youtube.</td>
+                        <td><a href="https://www.youtube.com/watch?v=4NtegAOQpSs" target="_blank">Bijentaal</a></td>
                     </tr>
                     </tbody>
                 </table>
