f9326ed33d65a34a46aeb0ebdf133592ad6931ec
MathieuvanBeek <74538092+MathieuvanBeek@users.noreply.github.com>
2022-06-20 10:33:10 +0200

    Basiscursus pagina aangemaakt

---
+++ resources/views/basiscursus.blade.php
@@ -0,0 +1,13 @@
+<x-guest-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Basiscursus') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <img src="basiscursus_2013_1.jpg" alt="Basiscursus 2013" width="500" height="600">
+        </div>
+    </div>
+</x-guest-layout>
