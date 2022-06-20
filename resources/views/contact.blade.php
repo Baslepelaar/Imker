ce015f7f0c7181e75f480bbb40da3350c9baac65
Noortje <noortjesteenbergen@gmail.com>
2022-06-20 10:42:56 +0200

    Update contact.blade.php

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,15 +1,100 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('het loopt als een tiet') }}
+            {{ __('') }}
         </h2>
-    </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                    <div class="main-container grid md:grid-cols-2">
+
+                        <article id="page-243" class="post-243 page type-page status-publish hentry">
+
+                            <header class="post-header"
+
+                                    style="font-size: 22px;
+                                    color: yellow;
+                                    font-family: Arial;
+                                    text-align: center;
+                                    margin-bottom: 17px;
+                                    font-size: 22px;
+                                    line-height: 34px;
+                                    letter-spacing: 0.5px;
+                                    font-weight: 600;
+                                    text-transform: uppercase;
+                                    margin-bottom: 8px;">
+                                <h1 class="page-title">Contact</h1>
+                            </header>
+
+                            <article class="post-content" style="text-align: center">
+                                <p>Het bestuur van de Imkervereniging Oegstgeest bestaat uit:</p>
+
+                                <table class="wp-block-table has-fixed-layout is-style-stripes" style="">
+                                    <tbody>
+                                    <tr>
+                                        <td>Alma van der Wijgaard</td>
+                                        <td>voorzitter</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Fred Schalker</td>
+                                        <td>secretaris</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Karel Diephuis</td>
+                                        <td>penningmeester</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Håvar Solheim</td>
+                                        <td>bestuur lid</td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+
+                                <p>Voor vragen over de vereniging, of onze cursus, verstuur een mail naar onze <a href="mailto:secretaris@imkervereniging-oegstgeest.nl">secretaris</a>.</p>
+
+                                <p>Voor vragen over de website zelf, neem contact op met de <a href="mailto:webmaster@imkervereniging-oegstgeest.nl">webmaster</a>.</p>
+                        </article>
+                        <div>
+                            <aside class="sidebar-right" style="float:right">
+
+                                <div id="recent-posts-4" class="bard-widget widget_recent_entries">
+                                    <div class="widget-title" style="border-top-color: #fcb400;"><h4>Recente berichten</h4></div>
+                                    <ul style="box-sizing: border-box;">
+                                        <li style="border-bottom-width: 1px;
+                                            border-bottom-style: dotted;
+                                            line-height: 2.1;
+                                            padding: 7px 0;">
+                                            <a href="https://www.imkervereniging-oegstgeest.nl/2022/06/09/honing-slingeren/">Honing slingeren</a>
+                                        </li>
+                                        <li>
+                                            <a href="https://www.imkervereniging-oegstgeest.nl/2022/02/21/bijenmarkt-austerlitz/">Bijenmarkt Austerlitz</a>
+                                        </li>
+                                        <li>
+                                            <a href="https://www.imkervereniging-oegstgeest.nl/2021/11/04/diplomas-uitgereikt/">Diploma’s uitgereikt!</a>
+                                        </li>
+                                        <li>
+                                            <a href="https://www.imkervereniging-oegstgeest.nl/2021/11/01/cursus-specialist-bijenproducten-haarlem/">Cursus Specialist Bijenproducten – Haarlem</a>
+                                        </li>
+                                        <li>
+                                            <a href="https://www.imkervereniging-oegstgeest.nl/2021/08/31/slingeren/">Slingeren</a>
+                                        </li>
+                                        <li>
+                                            <a href="https://www.imkervereniging-oegstgeest.nl/2021/07/12/bijenjournaal/">Bijenjournaal</a>
+                                        </li>
+                                    </ul>
+
+                                </div>	</aside>
+                        </div>
+                            </div>
+
             </div>
+
         </div>
-    </div>
+
+
+
+
+
+    </x-slot>
 </x-app-layout>
