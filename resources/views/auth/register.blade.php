cfe66d7a5635f8b086fcf973c3d5f5591a08e19a
fewpixels <74591245+fewpixels@users.noreply.github.com>
2022-07-03 18:54:40 +0200

    final adjustments
    slights adjustments in order to match styling a bit better

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,7 @@
 <x-guest-layout>
     <x-jet-authentication-card>
         <x-slot name="logo">
-            <x-jet-authentication-card-logo />
+            <img src="../img/bee.png" alt="een bij" class="block w-auto" style="height:150px">
         </x-slot>
 
         <x-jet-validation-errors class="mb-4" />
